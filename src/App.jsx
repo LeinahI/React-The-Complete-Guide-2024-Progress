811e3e545073087f28c1723a956622b30c8783d6
Leinah <gtpndn@gmail.com>
2024-02-18 18:55:09 +0900

    Deriving & Outputting Data Based on State

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,18 @@ import { useState } from "react";
 
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
-import { CORE_CONCEPTS } from "./data.js";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import jsxImg from "./assets/jsx-ui.png";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  let [ selectedTopic, setSelectedTopic ] = useState('click mo to');
+  let [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton)
-    console.log(selectedTopic);
+    setSelectedTopic(selectedButton);
   }
 
-  console.log('App component executing');
+  console.log("App component executing");
   return (
     <>
       <Header />
@@ -50,7 +49,14 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </>
