acd16a2525a32b937ba357daae093a7ec2ee02c6
Leinah <gtpndn@gmail.com>
2024-02-23 03:18:04 +0900

    Forwarding Props to wrapped elements by using spread operator

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? "active" : ""} onClick={onSelect}>
+      <button className={isSelected ? "active" : ""} {...props}>
         {children}
       </button>
     </li>
