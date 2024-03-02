ed41c07e2ad68b73fa702172882f0afb01cd6299
Leinah <gtpndn@gmail.com>
2024-03-02 11:02:21 +0900

    Why Immutability Matters

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,11 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  // Initialize the game board
-  let gameBoard = initGameBoard;
+  
+  //Create a new gameBoard array by using the spread operator to make a deep copy of the initGameBoard array. 
+  //This ensures that the new gameBoard array is a separate copy from the original initGameBoard array, 
+  //and any changes made to the new array will not affect the original array.
+  let gameBoard = [...initGameBoard.map((array) => [...array])];
 
   // Loop through each turn in the game
   for (const turn of gameTurns) {
@@ -45,13 +48,16 @@ function App() {
   // Loop through each winning combination
   for (const combination of WINNING_COMBINATIONS) {
     // Retrieve the symbol at the position specified by the row and column of the first element in the combination array
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
 
     // Retrieve the symbol at the position specified by the row and column of the second element in the combination array
-    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
 
     // Retrieve the symbol at the position specified by the row and column of the third element in the combination array
-    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
 
     // Check if there is a winner based on the symbols in the current combination
     if (
@@ -64,6 +70,7 @@ function App() {
     }
   }
 
+  // Check if the number of game turns is 9 and there is no winner
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -77,6 +84,12 @@ function App() {
 
       return updatedTurns;
     });
+  }
+
+  // Define a function called handleRematch
+  function handleRematch() {
+    // Set the game turns array to an empty array
+    setGameTurns([]);
   }
 
   return (
@@ -96,7 +109,10 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {/* If there is a winner or a draw, render the GameOver component with the handleRematch function to allow for a rematch */}
+        {(winner || hasDraw) && (
+            <GameOver onRematch={handleRematch} winner={winner} />
+        )}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
