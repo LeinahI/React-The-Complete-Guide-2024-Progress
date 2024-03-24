909467c692e88d4a179407d2d203a431dccdad0f
Leinah <gtpndn@gmail.com>
2024-03-24 19:24:07 +0900

    Understanding React Error Messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
-import Header from "./Components/Header/Header";
-import UserInput from "./Components/UserInput/UserInput";
-import Results from "./Components/Results/Results";
-import { useState } from "react";
+import { useState } from 'react';
+
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 function App() {
-  // Declare a state variable userInput and a function setUserInput to update it
   const [userInput, setUserInput] = useState({
-    initialInvestment: 16000,
+    initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
 
-  // Check if all input values are greater than or equal to 1
-  const inputIsValid =
-    userInput.initialInvestment >= 1 &&
-    userInput.annualInvestment >= 1 &&
-    userInput.expectedReturn >= 1 &&
-    userInput.duration >= 1;
-
-  // Define a function handleChange that takes an object with inputIdentifier and newVal properties as its argument
-  function handleChange(inputIdentifier, newVal) {
-    // Update the state variable userInput using setUserInput function
+  function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
-      // Return a new object with all properties from prevUserInput, and update the property specified by inputIdentifier with newVal
       return {
         ...prevUserInput,
-        [inputIdentifier]: +newVal, //If you add a "+" on newVal, it would concatenate the newVal with the existing value of newVal in the state variable userInput[inputIdentifier]. This is assuming newVal is a string, as the behavior of the "+" operator changes based on the data types involved.
+        [inputIdentifier]: newValue,
       };
     });
   }
@@ -34,19 +24,8 @@ function App() {
   return (
     <>
       <Header />
-      <UserInput UserInputProp={userInput} onChangeProp={handleChange} />
-      {!inputIsValid ? (
-        <p className="center">
-          Please enter the&nbsp;
-          {userInput.initialInvestment < 1 && "initial investment, "}
-          {userInput.annualInvestment < 1 && "annual investment, "}
-          {userInput.expectedReturn < 1 && "expected return, "}
-          {userInput.duration < 1 && "duration, "}
-          greater than zero
-        </p>
-      ) : (
-        <Results input={userInput} />
-      )}
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   );
 }
--- src/util/investment.js
+++ src/util/investment.js
@@ -9,22 +9,19 @@ export function calculateInvestmentResults({
   annualInvestment,
   expectedReturn,
   duration,
-}) {
-  const annualData = [];
+}, results) {
   let investmentValue = initialInvestment;
 
   for (let i = 0; i < duration; i++) {
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
-    annualData.push({
+    results.push({
       year: i + 1, // year identifier
       interest: interestEarnedInYear, // the amount of interest earned in this year
       valueEndOfYear: investmentValue, // investment value at end of year
       annualInvestment: annualInvestment, // investment added in this year
     });
   }
-
-  return annualData;
 }
 
 // The browser-provided Intl API is used to prepare a formatter object
@@ -32,7 +29,7 @@ export function calculateInvestmentResults({
 // Example Usage: formatter.format(1000) => yields "$1,000"
 export const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
-  currency: 'PHP', //USD
+  currency: 'PHP',
   minimumFractionDigits: 0,
   maximumFractionDigits: 0,
 });
